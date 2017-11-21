181fa0661e6cddcfdca722919a3c2f0fbaf3b179
Daiane Bushey <daianebushey@gmail.com>
2017-11-21 19:07:17 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,23 +4,32 @@ class TicTacToe
   end
 
   WIN_COMBINATIONS = [
-  [0,1,2], # Top row
-  [3,4,5], # Middle row
-  [6,7,8], # Bottom row
-  [0,3,6], # Left column
-  [1,4,7], # Middle column
-  [2,5,8], # Right column
-  [0,4,8], # Diagonal 1
-  [6,4,2]  # Diagonal 2
-]
+    [0,1,2], # Top row
+    [3,4,5], # Middle row
+    [6,7,8], # Bottom row
+    [0,3,6], # Left column
+    [1,4,7], # Middle column
+    [2,5,8], # Right column
+    [0,4,8], # Diagonal 1
+    [6,4,2]  # Diagonal 2
+  ]
 
-def display_board
-  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-  puts "-----------"
-  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-  puts "-----------"
-  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-end
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(input)
+    index = input.to_i - 1
+  end
+
+  def move(index, token = "X")
+    @board[index] = token
+  end
+
 
   def current_player
     turn_count % 2 == 0 ? "X" : "O"
